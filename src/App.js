0879a40e2d425644c3564c9ea763d81c16fcbd58
Anwar <gamraanwar@gmail.com>
2021-05-27 16:00:40 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import React from 'react';
+import MovieList from './component/MovieList';
+
+
+function App() {
+
+  
+  
+  return (
+    <div className="App">
+      <header  className="App-header">
+      <MovieList/>
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Filter.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import ReactStars from "react-rating-stars-component";
+
+const SearchText = ({filtertext,filterrate}) => {
+    return (
+        <div style = {{display:"flex", justifyContent: "space-around", alignItems:"center"}}>
+            <h1 style={{color:"red" , fontSize:"80px"}} className="titre"><span style={{color:"white"}}>Anwar</span>Card</h1>
+        <div style = {{display:"flex"}}>
+            <form className="search">
+        <input
+          onChange={(event)=>filtertext(event.target.value)}
+          placeholder='Search Movies'
+
+          type="text"
+        />
+
+      </form>
+      <ReactStars
+    count={5}
+    onChange={(newRating)=>filterrate(newRating)}
+    size={24}
+    activeColor="#ffd700"
+  />,
+        </div>
+        </div>
+    )
+}
+
+export default SearchText
